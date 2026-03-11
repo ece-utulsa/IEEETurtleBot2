54ffe18750332fb88a8a4e514ac711a7eb41a2f4
Hannah3141 <hannahmalini@gmail.com>
2026-03-11 01:54:58 +0000

    idrk care about this commit, just make git happy

--- src/turtlebot3/test_auto/test_auto/comp_auto.py
+++ src/turtlebot3/test_auto/test_auto/comp_auto.py
@@ -74,6 +74,7 @@ class Turtlebot3RelativeMove(Node):
 =======
     states = [(intom(-12), 0, 0, "x"), (intom(-12), 0, degtorad(-90), "theta"), (intom(-12), intom(-12), degtorad(-90), "y"), (intom(-12), intom(-12), degtorad(-180), "theta")] #, (0, intom(-12), degtorad(-180), "x")] #, (0, intom(-12), degtorad(90), "theta"), (0, 0, degtorad(90), "y"), (0, 0, 0, "theta")]
 >>>>>>> Stashed changes
+    states = [(intom(-12), 0, 0, "x"), (intom(-12), 0, degtorad(-90), "theta"), (intom(-12), intom(-12), degtorad(-90), "y"), (intom(-12), intom(-12), degtorad(-180), "theta")] #, (0, intom(-12), degtorad(-180), "x")] #, (0, intom(-12), degtorad(90), "theta"), (0, 0, degtorad(90), "y"), (0, 0, 0, "theta")]
     state = 0
     states = [(intom(12), 0, 0, "x")]
 
