13caaa8224d84e66a9eca767bffdf3907c379a3b
mauratoney <156852328+mauratoney@users.noreply.github.com>
2026-03-16 00:04:32 -0500

    Create controlArduino2.py

---
+++ IEEE_Robot_2026/controlArduino2.py
@@ -0,0 +1,64 @@
+import serial
+import time
+
+# --- Setup serial port ---
+arduino_port = "/dev/ttyACM0"
+baud_rate = 115200
+ser = serial.Serial(arduino_port, baud_rate, timeout=1)
+
+time.sleep(2)  # wait for Arduino to reset
+
+# --- Command functions ---
+def move_motor(steps: int, direction: int):
+    if direction not in (0, 1): # 0 = down, 1 = up
+        raise ValueError("Direction must be 0 or +1.")
+    if direction == 1:
+        direction_byte = 0x01
+        direction_str = "up"
+    else:
+        direction_byte = 0x00
+        direction_str = "down"
+    ser.write(bytes([0xFF, 0x01, steps, direction_byte]))
+    ack = ser.read()
+    if ack == b'\xAA':
+        print(f"Motor moved {steps} steps {direction_str}")
+    else:
+        print("Error moving motor")
+
+def move_motor_full(direction: int):
+    if direction not in (0,1): # 0 = down, 1 = up
+        raise ValueError("Direciton must be 0 or +1.")
+    ser.write(bytes([0xFF, 0x2, direction, 0x00]))
+    if ack == b'\xAA':
+        print(f"Motor moved full distance")
+    else:
+        print("Error moving motor")
+
+def turn_servos(direction: int):
+    if direction not in (0,1,2):
+        raise ValueError ("Direction must be 0 or +1 or +2 (in and out)")
+    ser.write(bytes([0xFF, 0x03, direction, 0x00]))
+    ack = ser.read()
+    if ack == b'\xAA':
+        print(f"Servos turned")
+    else:
+        print("Error turning servos")
+
+def set_relay(on: bool):
+    ser.write(bytes([0xFF, 0x04, 0x01 if on else 0x00, 0x00]))
+    ack = ser.read()
+    if ack == b'\xAA':
+        print(f"Relay {'ON' if on else 'OFF'}")
+    else:
+        print("Error setting relay")
+
+# --- Example Usage ---
+move_motor(250, 1)  # move 1000 steps up
+time.sleep(2)
+move_motor(250, 0)  # move 20 steps down
+time.sleep(2)
+turn_servos(0)
+time.sleep(2)
+turn_servos(1)
+time.sleep(2)
+turn_servos(2)
