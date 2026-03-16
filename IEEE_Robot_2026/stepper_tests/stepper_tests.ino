1d4efd1b7b3309fa2ce1d3ef3aecc154c4890516
mauratoney <156852328+mauratoney@users.noreply.github.com>
2026-03-15 22:43:24 -0500

    Update stepper_tests.ino

--- IEEE_Robot_2026/stepper_tests/stepper_tests.ino
+++ IEEE_Robot_2026/stepper_tests/stepper_tests.ino
@@ -1,64 +1,27 @@
-#include <Arduino.h>
-#include <Servo.h>
+//SLAVE CODE
 
-// Stepper Motor
-#define ENA_1 2
-#define STEP_1 4
-#define DIR_1 3
+#include <SPI.h>
 
-// Servos
-Servo leftServo;
-Servo rightServo;
-int leftPos = 0;
-int rightPos = 0;
+//Received byte from Master
+volatile byte recv_byte;
 
 void setup() {
-  pinMode(ENA_1, OUTPUT);
-  pinMode(STEP_1, OUTPUT);
-  pinMode(DIR_1, OUTPUT);
+  // put your setup code here, to run once:
+  Serial.begin(115200); // for debugging.
 
-  digitalWrite(ENA_1, LOW);
-  leftServo.attach(8);
-  rightServo.attach(9);
-}
+  // Turn ON SPI in slave mode.
+  SPCR |= bit(SPE);
+  // Have to send on MASTER IN, SLAVE OUT
+  pinMode(MISO,OUTPUT);
+} // end of setup
 
 void loop() {
-  motorstep(200, 1);  // Up
-  turnServos(0);
-  motorstep(200, 0);  //Down
-  turnServos(1);
-}
+  
+  //Byte Received!
+  if ((SPSR & (1 << SPIF)) != 0)
+  {
+    recv_byte = SPDR;
+  }  
+    Serial.println(recv_byte);
 
-void motorstep(int numSteps, int direction) {
-  if (direction == 0) {
-    digitalWrite(DIR_1, LOW);
-  } else {
-    digitalWrite(DIR_1, HIGH);
-  }
-  for (int x = 0; x < numSteps; x++) {
-    digitalWrite(STEP_1, HIGH);
-    delay(1);  // by changing this time delay between the steps we can change the rotation speed
-    digitalWrite(STEP_1, LOW);
-    delay(1);
-  }
-}
-
-void turnServos(int direction) {
-  if (direction == 0) {
-    rightPos = 180;
-    for (leftPos = 0; leftPos < 180; leftPos++) {
-      leftServo.write(leftPos);
-      rightServo.write(rightPos);
-      rightPos--;
-      delay(15);
-    }
-  } else {
-    leftPos = 180;
-    for (rightPos = 0; rightPos < 180; rightPos++) {
-      leftServo.write(leftPos);
-      rightServo.write(rightPos);
-      leftPos--;
-      delay(15);
-    }
-  }
 }
